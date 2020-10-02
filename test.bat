176d887dd34b12b5eb5e334831fc320cffa46a51
Power <powerfinnthegreat@gmail.com>
2020-10-02 18:45:57 +1000

    Yes?

---
+++ test.bat
@@ -0,0 +1,21 @@
+@echo off
+echo potato
+echo eeee
+echo sdiasdjoijodsifxjiofsd
+echo whyyy
+echo sdfoisdjoivjsinmfgklsdmf
+echo sdaopidjmfviksdoerjgmiksrejmflokiesjmdfklieds
+goto apple
+echo will this be seen
+echo nope
+echo not at all
+echo never in a million years
+echo not a single line
+echo not a one
+echo nothing
+echo nada
+echo n
+echo noooooooooooooooooope
+:apple
+echo nope
+pause
